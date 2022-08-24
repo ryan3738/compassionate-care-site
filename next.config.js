55e1fa717129484b340b9469bef3e2ebf0d5b447
ryan3738 <ryan3738@gmail.com>
2022-08-24 16:00:18 -0700

    add next.config.js file

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+};
